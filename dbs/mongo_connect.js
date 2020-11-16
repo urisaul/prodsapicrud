9df027fc598145c919dd869983b0bd29e347b26b
urisaul <64978916+urisaul@users.noreply.github.com>
2020-11-16 20:35:25 +0200

    Update mongo_connect.js

--- dbs/mongo_connect.js
+++ dbs/mongo_connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/lions', {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect('mongodb+srv://cliffb55:CliffHome99@cluster0.y5hvk.mongodb.net/lions', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://<username>:<password>@cluster0.y5hvk.mongodb.net/lions', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
